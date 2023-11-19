472abf00b3bc49cb029d1c3ee66e281047336cff
marquake <marcosms@gmail.com>
2023-11-19 21:02:08 +0100

    primera subida proyecto CRUD

---
+++ springCRUD001/src/test/java/com/mms/springCRUD001/SpringCrud001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mms.springCRUD001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrud001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
